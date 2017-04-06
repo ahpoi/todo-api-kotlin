916fbcecdc8790b69c64ec793e63b6f65429e952
Ian Ah Po <ian.ahpo@auspost.com.au>
2017-04-06 22:21:46 +1000

    Using let to check for null entity for the getTodo endpoint

--- src/main/kotlin/com/todo/TodoController.kt
+++ src/main/kotlin/com/todo/TodoController.kt
@@ -2,6 +2,7 @@ package com.todo
 
 import com.todo.model.Todo
 import com.todo.model.TodoRequest
+import com.todo.model.TodoResponse
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -11,15 +12,16 @@ import org.springframework.web.bind.annotation.*
 open class TodoController(val service: TodoService) {
 
     @PostMapping
-    fun createTodo(@RequestBody request: TodoRequest): ResponseEntity<Todo> {
+    fun createTodo(@RequestBody request: TodoRequest): ResponseEntity<TodoResponse> {
         val todo = service.saveTodo(Todo.fromRequestToEntity(request))
-        return ResponseEntity.status(HttpStatus.OK).body(todo)
+        return ResponseEntity.status(HttpStatus.OK).body(Todo.fromEntityToResponse(todo))
     }
 
     @GetMapping("/{uuid}")
-    fun getTodo(@PathVariable("uuid") id: String): ResponseEntity<Todo> {
-        val todo = service.findTodo(id)
-        return if (todo != null) ResponseEntity.status(HttpStatus.OK).body(todo) else ResponseEntity.notFound().build()
+    fun getTodo(@PathVariable("uuid") id: String): ResponseEntity<TodoResponse> {
+        val todo = service.findTodo(id)?.let { it } ?: return ResponseEntity.notFound().build()
+        return ResponseEntity.status(HttpStatus.OK).body(Todo.fromEntityToResponse(todo))
     }
-
 }
+
+
